4ea6f56c44a9fc36ef23eb5f1c625021c6cba411
eamorgado <39220282+eamorgado@users.noreply.github.com>
2019-03-19 17:06:10 +0000

    Update MinMaxNoPrun.java

--- Version2/MinMaxNoPrun.java
+++ Version2/MinMaxNoPrun.java
@@ -1,10 +1,12 @@
 import java.util.LinkedList;
+import java.util.Random;
 class MinMaxNoPrun{
     private int max_depth_pru;//Defines the maximum depth of search
     //The higher the depth the higher the difficulty
     private char player;
 
     public MinMaxNoPrun(int m_depth,char player){
+        //Here, player is the AI player
         this.max_depth_pru=m_depth;
         this.player=player;
     }
@@ -16,39 +18,65 @@ class MinMaxNoPrun{
     public void setPlayer(char p){this.player=p;}
 
     public Play minMax(Table tb){
-        if(tb.getPlayer()=='X')//Maximize
-            return max(new Table(tb),0);
-        else//Minimize
-            return min(new Table(tb),0);
-    }
-    public Play max(Table tb,int depth){
-        if(tb.isGameOver() || depth==max_depth_pru){
-            Play p = new Play(tb.getPlay().getRow(),tb.getPlay().getCol(),tb.utility());
+        if(getPlayer()=='X'){//Maximize Value of x
+            Play p= max(new Table(tb),0);
+            System.out.println(p.getUtility());
             return p;
         }
-        Play maxPlay = new Play(Integer.MIN_VALUE);
+        else{//Minimize value of O
+            Play p= min(new Table(tb),0);
+            System.out.println(p.getUtility());
+            return p;
+        }
+    }
+    public Play max(Table tb,int depth){
+        Random rand_play = new Random();
+        if(tb.isGameOver() || depth>max_depth_pru){
+            Play p = new Play(tb.getPlay().getRow(),tb.getPlay().getCol(),tb.utility());
+            //if(tb.isGameOver())System.out.println(p.getUtility());
+            return p;
+        }
+        Play maxPlay = new Play(Integer.MIN_VALUE);//we nwant to maximize X value
         for(Table son : tb.getDescendents('X')){
             Play p = min(son,depth+1);
+            //System.out.println(p.getUtility());
             if(p.getUtility()>maxPlay.getUtility()){
                 maxPlay.setRow(son.getPlay().getRow());
                 maxPlay.setCol(son.getPlay().getCol());
-                maxPlay.setUtility(son.getPlay().getUtility());
+                maxPlay.setUtility(p.getUtility());
+            }
+            else if(p.getUtility()==maxPlay.getUtility()){
+                if(rand_play.nextInt(2)==0){
+                    maxPlay.setRow(son.getPlay().getRow());
+                    maxPlay.setCol(son.getPlay().getCol());
+                    maxPlay.setUtility(p.getUtility());
+                }
             }
         }
         return maxPlay;
     }
     public Play min(Table tb,int depth){
+        Random rand_play = new Random();
+
         if(tb.isGameOver() || depth==max_depth_pru){
             Play p = new Play(tb.getPlay().getRow(),tb.getPlay().getCol(),tb.utility());
             return p;
         }
         Play minPlay = new Play(Integer.MAX_VALUE);
+        //System.out.println(p.getUtility());
         for(Table son : tb.getDescendents('O')){
             Play p = max(son,depth+1);
             if(p.getUtility()<minPlay.getUtility()){
                 minPlay.setRow(son.getPlay().getRow());
                 minPlay.setCol(son.getPlay().getCol());
-                minPlay.setUtility(son.getPlay().getUtility());
+                minPlay.setUtility(p.getUtility());
+            }
+            else if(p.getUtility()==minPlay.getUtility()){
+                if(rand_play.nextInt(2)==0){
+                    minPlay.setRow(son.getPlay().getRow());
+                    minPlay.setCol(son.getPlay().getCol());
+                    minPlay.setUtility(p.getUtility());
+                }
             }
         }
         return minPlay;
